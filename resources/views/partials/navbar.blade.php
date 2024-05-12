d6af66e82f25da148a8824e7377ab2abe1d4f2a4
King Agung <kingagungks23@gmail.com>
2024-05-13 06:55:54 +0700

    change title in main.blade.php and vavbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-danger">
     <div class="container">
-      <a class="navbar-brand" href="/">myoit-3</a>
+      <a class="navbar-brand" href="/">Tugas 19 IOT Frontend</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
