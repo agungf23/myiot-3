fca4ded747225c4d5e1fdac5fb70ee7cf8a7474f
King Agung <kingagungks23@gmail.com>
2024-05-12 17:35:23 +0700

    move navbar to views/partials/navbar.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,30 +8,7 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg bg-danger">
-        <div class="container">
-          <a class="navbar-brand" href="/">myoit-3</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active"  href="/dashboard">Dashboard</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/devices">Devices</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/rules">Rules</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/users" >Users</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    @include('partials.navbar')
 
     <div class="container mt-5">
         @yield('container')
