b59750e3f9e5e13181f03bce4a4e68fbc65ff516
King Agung <kingagungks23@gmail.com>
2024-05-12 18:27:57 +0700

    use devices dummy data and show the data in devices.blade.php as a table and each record can be clickciked and move to single device view

--- routes/web.php
+++ routes/web.php
@@ -3,35 +3,165 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard', [
+        "title" => "dashboard"
+    ]);
 });
 
-// dashboard
+//dashboard
 Route::get('/dashboard', function () {
     return view('dashboard', [
         "title" => "dashboard"
     ]);
 });
 
-// devices
+//devices
 Route::get('/devices', function () {
+    $devices = [
+        [
+            "id" => 1,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ],
+        [
+            "id" => 2,
+            "name" => "Kipas Angin",
+            "min_value" => 0,
+            "max_value" => 5,
+            "current_value" => 2
+        ],
+        [
+            "id" => 3,
+            "name" => "Lampu Kamar",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 50
+        ],
+        [
+            "id" => 4,
+            "name" => "Lampu Taman",
+            "min_value" => 0,
+            "max_value" => 1,
+            "current_value" => 1
+        ],
+        [
+            "id" => 8,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ],
+        [
+            "id" => 9,
+            "name" => "Kipas Angin",
+            "min_value" => 0,
+            "max_value" => 5,
+            "current_value" => 2
+        ],
+        [
+            "id" => 13,
+            "name" => "Lampu Kamar",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 50
+        ],
+        [
+            "id" => 15,
+            "name" => "Lampu Taman",
+            "min_value" => 0,
+            "max_value" => 1,
+            "current_value" => 1
+        ]
+    ];
     return view('devices', [
         "title" => "devices",
-        "name" => "sensor suhu",
-        "max_value" => 100,
-        "min_value" => 0,
-        "current_value" => 25
+        "devices" => $devices
     ]);
 });
 
-// rules
+Route::get('/devices/{id}', function ($id) {
+    $devices = [
+        [
+            "id" => 1,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ],
+        [
+            "id" => 2,
+            "name" => "Kipas Angin",
+            "min_value" => 0,
+            "max_value" => 5,
+            "current_value" => 2
+        ],
+        [
+            "id" => 3,
+            "name" => "Lampu Kamar",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 50
+        ],
+        [
+            "id" => 4,
+            "name" => "Lampu Taman",
+            "min_value" => 0,
+            "max_value" => 1,
+            "current_value" => 1
+        ],
+        [
+            "id" => 8,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ],
+        [
+            "id" => 9,
+            "name" => "Kipas Angin",
+            "min_value" => 0,
+            "max_value" => 5,
+            "current_value" => 2
+        ],
+        [
+            "id" => 13,
+            "name" => "Lampu Kamar",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 50
+        ],
+        [
+            "id" => 15,
+            "name" => "Lampu Taman",
+            "min_value" => 0,
+            "max_value" => 1,
+            "current_value" => 1
+        ]
+    ];
+
+    $select_device = [];
+    foreach($devices as $device){
+        if($device["id"] == $id){
+            $select_device = $device;
+        }
+    };
+
+    return view('device', [
+        "title" => "device",
+        "device" => $select_device
+    ]);
+});
+
+//rules
 Route::get('/rules', function () {
     return view('rules', [
         "title" => "rules"
     ]);
 });
 
-// users
+//users
 Route::get('/users', function () {
     return view('users', [
         "title" => "users"
