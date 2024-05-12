b86e1182bba6650064689c37ff96df63f835b45e
King Agung <kingagungks23@gmail.com>
2024-05-12 15:50:00 +0700

    add views: dashboard, devices, rules, users and edit routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+// devices
+Route::get('/devices', function () {
+    return view('devices');
+});
+
+// rules
+Route::get('/rules', function () {
+    return view('rules');
+});
+
+// users
+Route::get('/users', function () {
+    return view('users');
+});
