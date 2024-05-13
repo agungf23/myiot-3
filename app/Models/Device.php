380ccd65688f02712ddc18d2f6ccc68c94830128
King Agung <kingagungks23@gmail.com>
2024-05-13 08:43:36 +0700

    commit

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -2,44 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Device extends Model
-// {
-//     use HasFactory;
-// }
-
-class Device{
-    private static $devices = [
-        [
-            "id" => 1,
-            "name" => "Sensor Suhu"
-        ],
-        [
-            "id" => 2,
-            "name" => "Kipas Angin"
-        ],
-        [
-            "id" => 3,
-            "name" => "Lampu Kamar"
-        ],
-        [
-            "id" => 4,
-            "name" => "Lampu Taman"
-        ],
-        [
-            "id" => 5,
-            "name" => "Sensor Suhu"
-        ]
-    ];
-
-    public static function all(){
-        return collect(self::$devices);
-    }
-
-    public static function show($id){
-        $devices = static::all();
-        return $devices->firstWhere('id', $id);
-    }
+class Device extends Model
+{
+    use HasFactory;
 }
