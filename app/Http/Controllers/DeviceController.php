062fa49a05c1f0a91a680cfec8837b88dcd20db8
King Agung <kingagungks23@gmail.com>
2024-05-13 11:05:15 +0700

    commit again

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -8,62 +8,51 @@ use Illuminate\Http\Request;
 class DeviceController extends Controller
 {
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Device::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $device = new Device;
         $device->name = $request->name;
+        $device->min_value = $request->min_value;
+        $device->max_value = $request->max_value;
+        $device->current_value = $request->current_value;
         $device->save();
-        return response()->json([
-        "message" > "Device telah ditambahkan."
-        ], 201);
+        return response()->json(["message" > "Device Added."], 201);
 
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         return Device::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         if (Device::where('id', $id)->exists()) {
             $device = Device::find($id);
             $device->name = is_null($request->name) ? $device->name : $request->name;
+            $device->min_value = is_null($request->min_value) ? $device->min_value : $request->min_value;
+            $device->max_value = is_null($request->max_value) ? $device->max_value : $request->max_value;
+            $device->current_value = is_null($request->current_value) ? $device->current_value : $request->current_value;
             $device->save();
-            return response()->json(["message" > "Device telah diupdate."], 201);
+            return response()->json(["message" > "Device updated."], 201);
             } else {
-                return response()->json(["message" > "Device tidak ditemukan."], 404);
+                return response()->json(["message" > "Device not found."], 404);
             }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         if (Device::where('id', $id)->exists()) {
             $device = Device::find($id);
             $device->delete();
-            return response()->json(["message" > "Device telah dihapus."], 201);
+            return response()->json(["message" > "Device deleted."], 201);
         } else {
-            return response()->json(["message" > "Device tidak ditemukan."], 404);
+            return response()->json(["message" > "Device not found."], 404);
         }
     }
 
