c0dd0aa9221ceae2a66665b1163a12db59922f6f
King Agung <kingagungks23@gmail.com>
2024-05-13 07:14:46 +0700

    add data log

--- routes/web.php
+++ routes/web.php
@@ -99,16 +99,86 @@ Route::get('/devices/{id}', function ($id) {
     ]);
 });
 
-//rules
-Route::get('/rules', function () {
-    return view('rules', [
-        "title" => "rules"
+//logs
+Route::get('/logs', function () {
+    $logs = [
+        [
+            "id" => 1,
+            "name" => "Sensor Suhu",
+        ],
+        [
+            "id" => 2,
+            "name" => "Kipas Angin",
+        ],
+        [
+            "id" => 3,
+            "name" => "Lampu Kamar",
+        ],
+        [
+            "id" => 4,
+            "name" => "Lampu Taman",
+        ],
+        [
+            "id" => 5,
+            "name" => "Sensor Suhu",
+        ]
+
+    ];
+    return view('logs', [
+        "title" => "logs",
+        "devices" => $logs
     ]);
 });
 
-//users
-Route::get('/users', function () {
-    return view('users', [
-        "title" => "users"
+Route::get('/logs/{id}', function ($id) {
+    $logs = [
+        [
+            "id" => 1,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ],
+        [
+            "id" => 2,
+            "name" => "Kipas Angin",
+            "min_value" => 0,
+            "max_value" => 5,
+            "current_value" => 2
+        ],
+        [
+            "id" => 3,
+            "name" => "Lampu Kamar",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 50
+        ],
+        [
+            "id" => 4,
+            "name" => "Lampu Taman",
+            "min_value" => 0,
+            "max_value" => 1,
+            "current_value" => 1
+        ],
+        [
+            "id" => 5,
+            "name" => "Sensor Suhu",
+            "min_value" => 0,
+            "max_value" => 100,
+            "current_value" => 25
+        ]
+
+    ];
+
+    $select_log = [];
+    foreach($logs as $log){
+        if($log["id"] == $id){
+            $select_log = $log;
+        }
+    };
+
+    return view('log', [
+        "title" => "log",
+        "device" => $select_log
     ]);
 });
